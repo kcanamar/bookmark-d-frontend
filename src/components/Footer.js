6e5b634da2f8034cadc691bbbd1f36a034834363
Ben <ben.a.burgos@gmail.com>
2022-06-01 20:42:10 -0700

    adds style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,32 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  a {
+    text-decoration: none;
+  }
+`;
+
 export default function Footer() {
-  return <h1>This amazing website was made by Ben, Kyle, Nathan, and Joe.</h1>;
+  return (
+    <StyledFooter>
+      This amazing website was made by
+      <a href="https://github.com/sunghunp/" target="_blank" rel="noreferrer">
+        Joe
+      </a>
+      ,
+      <a href="https://github.com/kcanamar/" target="_blank" rel="noreferrer">
+        Kyle
+      </a>
+      ,
+      <a href="https://github.com/yinathan/" target="_blank" rel="noreferrer">
+        Nathan
+      </a>
+      , and
+      <a href="https://github.com/benburgos/" target="_blank" rel="noreferrer">
+        Ben
+      </a>
+      .
+    </StyledFooter>
+  );
 }
