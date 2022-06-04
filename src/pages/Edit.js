20e9f293bf5a29ed3108f9be16e71c8123356eeb
Joe Park <sunghun.josephp@gmail.com>
2022-06-04 12:16:44 -0700

    Edit functionality done

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,12 +1,13 @@
-import Form from '../components/Form'
+import Form from '../components/EditForm'
 
 export default function Edit({ bookmark }) {
+  
   return(
     <div>
       <h1>Let's Edit your Bookmark</h1>
       <div>
         <h2>{bookmark.title} ({bookmark.url})</h2>
-        <Form />
+        <Form id={bookmark._id}/>
       </div>
     </div>
   )
